eaf11f8ccd9163840d0b2822e2b5ae3f00cf20ac
tanghaoran666 <18408230362@163.com>
2020-11-18 22:51:18 +0800

    p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelController1Test.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LevelController1Test {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_advanced_when_given_level_1() throws Exception {
+        mockMvc.perform(get("/level")).andExpect(status().isOk())
+                .andExpect(jsonPath("$", is("advanced")));
+    }
+}
